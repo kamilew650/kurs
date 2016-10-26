aa557d7b4a2b0a094b8016d930f5af189a2ab35b
kamil <kamil.lewandowski1324@gmail.com>
2016-10-26 19:12:43 +0200

    poczatek kursu

---
+++ KURSkonsola/KURSkonsola/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KURSkonsola
+{
+    public class Person
+    {
+
+        public int Age { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ktos = new Person();
+
+            ktos.Age.set();
+            //int pierwszaZmienna, druga;
+            //pierwszaZmienna = 3;
+            //druga = 7;
+            //if(pierwszaZmienna < druga)
+            //{
+            //    Console.Write("cos tam");
+            //}
+
+            //Console.Write(pierwszaZmienna);
+
+            //Console.ReadKey();
+        }
+    }
+}

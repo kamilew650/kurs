88b4e7f2a74f0cc1c8bffc638889b62f6ebc20c9
kamil <kamil.lewandowski1324@gmail.com>
2016-11-16 18:59:21 +0100

    obiektowosc cz 2

---
+++ KURSkonsola/KURSkonsola/Kolekcje.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace KURSkonsola
+{
+    class Kolekcje
+    {
+        /*
+         * generycznosc where T : class, interface
+         *
+         * delegaty kwantyfikator_dostepu dlegate typ_zwracany NazwaDelegatu(Typ Arg);  
+         * metoda inline
+         * funkcja/metoda anonimowa
+         * 
+         * lambda =>
+         * 
+         * zdarzenia-obudowany delegat
+         *  dostep event typ_delegatu NazwaZdazenia;
+         *  
+         * kolekcje
+         *
+        */
+    }
+}

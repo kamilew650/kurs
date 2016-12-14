9b4d1a688af567f06f609223c480fea25ccd0d91
kamil <kamil.lewandowski1324@gmail.com>
2016-12-14 23:52:42 +0100

    poczatek przygody asp.net

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b>default</b> action";
+        }
+        //
+        // GET: Helloworld/Welcome
+        public string Welcome(string name, int ID)
+        {
+            return HttpUtility.HtmlAttributeEncode("hello " + name + ID);
+        }
+    }
+}

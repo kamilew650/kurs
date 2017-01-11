1e1026623f8f552990c780edf5c2d9ea61166996
kamil <kamil.lewandowski1324@gmail.com>
2017-01-11 18:36:10 +0100

    kurs c# 7 chyba

---
+++ KURSkonsola/KURSkonsola/Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KURSkonsola
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+
+    //class Program1
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var p = Person.Parse("John");
+    //        p.Introduce("Mosh");
+    //    }
+    //}
+}
---
+++ KURSkonsola/KURSkonsola/Program2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace KURSkonsola
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+    }
+
+    public class Order
+    {
+
+    }
+
+
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"john");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}

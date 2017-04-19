a59d9864aeb895b66e69c5f20cbd29cb7aa2682b
kamil <kamil.lewandowski1324@gmail.com>
2017-04-19 02:13:01 +0200

    api to movies model is ready

---
+++ Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string DataAdded { get; set; }
+
+        public byte GenreId { get; set; }
+
+        public byte NumberInStock { get; set; }
+    }
+}
